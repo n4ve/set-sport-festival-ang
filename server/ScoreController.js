b2943266baaa1a3e888ad94e3077ec5c7c53edc2
Navee Sratthatad <navees@set.or.th>
2019-06-02 10:20:07 +0700

    Change start date

--- server/ScoreController.js
+++ server/ScoreController.js
@@ -35,7 +35,7 @@ module.exports = function(app, db, jsonParser){
     console.log("Registering endpoint: /api/around-the-world");
     app.get('/api/around-the-world', function(req, res){ 
         let team = "\'" + req.query.team + "\'";
-        let sql = `SELECT SUM(score) AS total FROM score WHERE team == ${team} AND date >= \"2019-04-01\" AND date <= \"2019-08-01\"`;
+        let sql = `SELECT SUM(score) AS total FROM score WHERE team == ${team} AND date >= \"2019-06-01\" AND date <= \"2019-08-01\"`;
         console.log(sql);
         db.all(sql , function(err, rows) {
             console.log(rows);
